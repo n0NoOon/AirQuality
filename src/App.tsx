afef5151bf8a76830a0504d22d85b5591892b14a
n0noOon <sittipat93@gmail.com>
2024-12-04 23:11:49 +0700

    config for react-router

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Root from "./pages/Root";
 import Home from "./pages/Home/Home";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-      ],
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Root />,
+        children: [
+          {
+            index: true,
+            element: <Home />,
+          },
+        ],
+      },
+    ],
+    { basename: import.meta.env.DEV ? "/" : "/AirQuality/" }
+  );
 
   return <RouterProvider router={router} />;
 }
